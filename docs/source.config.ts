57a0823c11e8a741fea91d78a39ea5dc882e582c
Valerii Strilets <valerii.strilets@gmail.com>
2025-11-03 00:47:05 +0200

    docs: restore twoslash

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -1,10 +1,27 @@
-import { defineDocs } from 'fumadocs-mdx/config'
+import { rehypeCodeDefaultOptions } from 'fumadocs-core/mdx-plugins'
+import { defineConfig, defineDocs } from 'fumadocs-mdx/config'
+import { transformerTwoslash } from 'fumadocs-twoslash'
 
 export const docs = defineDocs({
   dir: 'content/docs',
   docs: {
     postprocess: {
       includeProcessedMarkdown: true,
+    },
+  },
+})
+
+export default defineConfig({
+  mdxOptions: {
+    rehypeCodeOptions: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      transformers: [
+        ...(rehypeCodeDefaultOptions.transformers ?? []),
+        transformerTwoslash(),
+      ],
     },
   },
 })
